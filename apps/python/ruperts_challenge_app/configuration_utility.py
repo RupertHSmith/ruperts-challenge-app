a533cc3c2ca57bba5637d840133f121064d40a8c
Rupert Smith <smith.rupert@outlook.com>
2022-02-21 20:16:53 +0000

    [See #2] Speed unit switchable and add configuration file utility

---
+++ apps/python/ruperts_challenge_app/configuration_utility.py
@@ -0,0 +1,28 @@
+import json
+import ac
+
+class ConfigurationUtility:
+  def __init__(self, config_file_name):
+    self.config_file_name = config_file_name
+    self.config = {}
+    try:
+      with open(config_file_name) as file:
+        self.config = json.load(file)
+        ac.log("{}".format(self.config))
+    except FileNotFoundError:
+      ac.log("File not found!")
+    except json.JSONDecodeError:
+      ac.log("Failed to parse JSON")
+  
+  def get_or_default(self, key, default):
+    if self.config and self.config[key]:
+      return self.config[key]
+    else: 
+      return default
+  
+  def set_value(self, key, value):
+    self.config[key] = value
+  
+  def save_config(self):
+    with open(self.config_file_name, "w") as output_file:
+      json.dump(self.config, output_file)
